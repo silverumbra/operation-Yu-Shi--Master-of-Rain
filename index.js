17b3018155750f55330311f0aa9d7cbba4813317
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-10 22:03:35 +0100

    added date index in JS (per diem)

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes <10) {
+        minutes = `0${minutes}`;
+    }
+
+    let perDiem = date.getDay();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[perDiem];
+
+    return `${day} ${hours} ${minutes}`;
+}

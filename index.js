41c22403d47fd8fa3ccea8d44de6b3e44c21c4f7
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-10 22:15:47 +0100

    added search function & api key

--- index.js
+++ index.js
@@ -31,7 +31,14 @@ function displayWeatherConditions(response) {
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
 }
 
+function search(event) {
+    event.preventDefault();
+    let apiKey = "edb378f0be539a4b2c5c96da53e9f0c1";
+    let city = document.querySelector("#city-input").value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeatherConditions);
 
+}
 
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();

fb2a4827d5a8fdfe67a2f7f6358514e8d9a4a18a
silverumbra <83974058+silverumbra@users.noreply.github.com>
2021-05-10 22:51:55 +0100

    added location event

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ function searchPosition(position) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(displayWeatherConditions);
+}
 
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 let dateElement = document.querySelector("#date");
